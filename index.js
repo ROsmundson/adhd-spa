acda7d23bee4ab6141d8d09fbc67b84ba474cd77
Ryan Osmundson <rosmundson@outlook.com>
2022-03-02 18:45:13 -0700

    commit created files

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Page has loaded");
+});
+
+//Templates
+
+function fetchTheFacts() {
+  fetch("http://localhost:3000/justTheFacts")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
+}
+
+fetchTheFacts()
