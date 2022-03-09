d8fdaa25c8e78f398078a6210d425fe3c29cb9ca
Ryan Osmundson <rosmundson@outlook.com>
2022-03-09 15:18:22 -0700

    committing changes to db.json

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:3000';
+const factsContainer = document.getElementById('fact-container');
+console.log(factsContainer);
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Page has loaded");
 });
 
-function renderTheFacts(factoid){
-    const factsContainer = document.getElementById('adhd-facts-container');
-    const factCard = document.createElement("div");
-    const factTitle = document.createElement("h3");
-    const factContent = document.createElement("p");
-    const factURL = document.createElement("a"); 
-    const nextBttn = document.createElement("button");
-    // const webSourcesDiv = document.createElement("div");
+//construct facts cards and append using randome
+function renderFacts(factoid){
+    const adhdCards = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const factsDiv = document.createElement("div");
     
-    factCard.id = `fact-${factoid.id}`
-    factCard.className = "fact-card";
-    factTitle.textContent = factoid.title;
-    factContent.textContent = factoid.fact;
-    factURL.className = "sources"
-    factURL.href = factoid.website;
-    nextBttn.id = "next"
-    nextBttn.className = "bttn";
-    nextBttn.innerText = "Next Randome Fact";
+    const factCardUl= document.createElement("ul");
+    const factContentLi = document.createElement("li");
+    const nextRandomeBttn = document.createElement("button")
     
+    factsContainer.id = "container";
+    factsContainer.class = "shadowbox";
+    adhdCards.id = `fact-${factoid.id}`;
+    adhdCards.className = "shadowbox";
+    h2.id = "fact-header";
+    h2.textContent = "Did you know?";
+    factsDiv.id = "facts-div";
+    factCardUl.className = "random-fact";
+    factCardUl.textContent = factoid.title;
+    factContentLi.className = "facts-list";
+    factContentLi.textContent = factoid.fact;
+    factContentLi.fact = factoid.fact;
+    nextRandomeBttn.id = "nextBttn";
+    nextRandomeBttn.className = "randomeBttn";
+    nextRandomeBttn.textContent = "Next Randome Fact";
+    
+    factsContainer.appendChild(adhdCards);
+    factsDiv.append()
+    adhdCards.append(h2, factsDiv, factCardUl, factContentLi, nextRandomeBttn)
+
+
+    // // const webSourcesDiv = document.createElement("div");
+    
+    // factTitle.textContent = factoid.title;
+    // factContent.textContent = factoid.fact;
+    // factURL.className = "sources"
+    // factURL.href = factoid.website;
     // webSourcesDiv.id = "source";
     // webSourcesDiv.class = "sources";
 
+   
 
-
-    
-
-    factsContainer.append(factCard);
-    factCard.append(factTitle, factContent, factURL, nextBttn);
+    // factCard.append(factTitle, factContent, factURL);
+    // factCard.append(nextRandomeBttn);
 }
 
 function fetchTheFacts() {
-    fetch(BASE_URL + '/justTheFacts')
+    fetch(BASE_URL + '/TheFacts')
     .then(response => response.json())
-    .then(data => data.forEach(renderTheFacts));
-}
+    .then(data => data.forEach(renderFacts));
+    };
 
 function initiateTheFacts() {
     fetchTheFacts();
