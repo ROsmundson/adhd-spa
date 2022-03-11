e4b795137fdd066f74b1f4179ae2b5b39ab2be09
Ryan Osmundson <rosmundson@outlook.com>
2022-03-11 13:05:39 -0700

    Commiting after failed attempt yesterday

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
+//Node grabbers
 const BASE_URL = 'http://localhost:3000';
 const homeNav = document.getElementById("home")
 const awayNav = document.getElementById("about-me")
 const factsContainer = document.getElementById("facts-container")
-
-//1. use tasklister as example for building a next iteration of facts as we did with todos?
+const userStoryContainer = document.getElementById("")
+const nextButton = document.createElement("button")
+//Page load notification
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Page has loaded");
 });
 
+//construct and prepare nodes to be rendered upon fetch
 function renderFacts(factoid){
     const factsCard = document.createElement("div");
     factsCard.id = "facts-card-title"
     factsCard.innerHTML = `<h3>${factoid.title}</h3>`;
-    
     const factsContent = document.createElement("p");
     factsContent.textContent = `${factoid.fact}`;
     
-    const p = document.createElement('p')
-    const nextButton = document.createElement("button");
-    nextButton.innerText = "Read Next Fact"
-    p.textContent = `${factoid}`
-    nextButton.addEventListener('click', handleSubmitEvent)
-
+    const p = document.createElement('p');
+    nextButton.innerText = "Read Next Fact";
+    p.textContent = `${factoid}`;
+    
     factsContainer.appendChild(factsCard);
     factsCard.append(factsContent);
-    factsCard.append(nextButton); 
-    }
-
-        
-//===============================================================
-
+    factsCard.append(nextButton);
+}
+    //===============================================================
+    
 function fetchTheFacts() {
     fetch(BASE_URL + '/TheFacts')
     .then(response => response.json())
-    .then(data => data.forEach(renderFacts));
-    };
+    .then(data => {
+        let fact = data
+        if(fact[0].id === 1){
+            renderFacts(fact[0]);
+        }
+        //on click move to next fact readNextFact()
+    })
+    .catch(error => console.error('Error:', error));
 
-function handleSubmitEvent() {
-    //placeholder
+    console.log(nextButton);
+    nextButton.addEventListener('click', () => {
+        console.log('i was clicked');
+        console.log(event);
+    });
+
+}
+
+function switchFacts(event){
+
 }
 
 function initiateTheFacts() {
     fetchTheFacts();
-    handleSubmitEvent();
 }
 
 initiateTheFacts()
+
+
+
+
+
+
+function renderUserStory() {console.log("returned fact")}
+
+
 
 
 
