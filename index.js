d5897dfb5eb56716354b6ef1e54ba1e8eeb4ce43
Ryan Osmundson <rosmundson@outlook.com>
2022-03-31 21:14:41 -0600

    commiting before changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //Main Node grabbers:
 const BASE_URL = 'http://localhost:3000';
 const factsContainer = document.getElementById("facts-container");
-const factsCard = document.getElementsByClassName("facts-cards")
+const factsCard = document.getElementsByClassName("facts-cards");
 
 
 //const homeNav = document.getElementById("home")
@@ -18,45 +18,50 @@ function renderFacts(factoid){
     const factsCard = document.createElement("div");
     factsCard.id = `fact-card-${factoid.id}`;
     factsCard.className = "facts-cards";
-    factsCard.style.display = 'none'
+    factsCard.style.display = 'none';
     const factsTitle = document.createElement("h3");
     factsTitle.textContent = `${factoid.title}`;
     const factsContent = document.createElement("p");
     factsContent.textContent = `${factoid.fact}`;     
-    const factsButton = document.getElementById("facts-button");
-    factsButton.id = "facts-button"
+    const factsButton = document.createElement("facts-button");
+    factsButton.id = `"fact-button-${factoid.id}`;
     factsButton.className = "facts-button";
     factsButton.textContent = "Read Next Fact";
     const br = document.createElement("br");
     factsContainer.append(factsCard);
     factsCard.append(factsTitle, factsContent);
     factsCard.appendChild(br);
-    factsContainer.append(factsButton);
+    factsCard.append(factsButton);
     }
 
 function fetchFacts() {
     fetch(BASE_URL + '/TheFacts')
     .then(response => response.json())
     .then(data => {
-        let facts = data;
-        facts.forEach(renderFacts);
-        
-        
-//         document.getElementById("okButton")
-//         .addEventListener("click", function() {
-//   document.getElementById("welcome").hidden = true;
-//   document.getElementById("awesome").hidden = false;
-// }, false);
-    })
+        data.forEach(renderFacts)
+        loadRemoveFacts(data);
+    });
 }
 
 function loadRemoveFacts(data) {
+    let facts = data
+    console.log(facts)
+    let welcome = document.getElementById("welcome");
+    let button = document.getElementById("welcome-button");
+    button.addEventListener('click', () => {
+        welcome.remove();
+        
+        
+        });
+    };
+        // let facts = data
+        // let factCard = document.getElementById(`fact-card-${facts.id}`)
+        //     factId = (factCard.id)
+        //     forEach(factIdconsole.log(factId))
+        //     })
+        // }
 
-}
-// let cars = ["Tesla", "Ferrari", "Lamborghini", "Audi"];
 
-// for(let i = 0; i < cars.length; i++) {
-//   console.log(cars[i]);
 
             
                 
