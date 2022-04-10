80c175aa3c191a057b2ae830098fa552ed728ed2
Ryan Osmundson <rosmundson@outlook.com>
2022-04-09 23:55:15 -0600

    pushing changes

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const factsContainer = document.getElementById("facts-container");
 const factsCards = document.getElementsByClassName("facts-cards");
 const factCard = document.getElementsByClassName("facts-cards")
 const cardsButton = document.createElement("button");
-cardsButton.textContent = "Read the Facts"
-const welcome = document.getElementById("welcome");
+cardsButton.textContent = "Read the Facts";
+const currentFact = document.getElementsByClassName("facts-cards");
+
 
 //const homeNav = document.getElementById("home")
 //const awayNav = document.getElementById("about-me")
@@ -29,33 +30,43 @@ function renderFacts(factoid){
     factsContainer.append(factsCards);
     factsCards.append(factsTitle, factsContent);
     factsContainer.appendChild(cardsButton);
-    }    
+}    
 
 function fetchFacts() {
     fetch(BASE_URL + '/TheFacts')
     .then(response => response.json())
-    .then(facts => loadRemoveFacts(facts));
+    .then(facts => {
+        loadCards(facts);
+        //removeCards(facts);
+    })
 }
 
-function loadRemoveFacts(facts) {
+function loadCards(facts) {
     let cards = facts
-    factsContainer.appendChild(cardsButton);
-    cardsButton.addEventListener("click", (e) => {
-        welcome.remove();
-        renderFacts(cards[0])
-        
-        cardsButton.textContent = 'Read Another Fact!'
-        for(let i = 0; i < facts.length; i++) {
-            cards = facts[i]
-            console.log(cards);
-            
-          
+        factsContainer.appendChild(cardsButton);
+        cardsButton.addEventListener("click", () => {
+            cardsButton.textContent = "Read Another Fact!";
+            for (let i = 0; i < cards.length; i++) {
+                console.log(factCard);
+                renderFacts(cards[i])
+                console.log(cards[i]);
+                console.log(factsContainer);
+                console.log(cardsButton);
 
-        }   
+            }
+
     })
 }
 
 
+function removeCards() {
+    
+}
+            // for(let i = 0; i < cardsButton; i++) {
+            //     cards.forEach(() => console.log(cards));
+            //         let card = facts[i];
+            //         let buttonId = `${card.id}`
+            // }
 
             
 
