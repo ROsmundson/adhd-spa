770dd12232a09643c50df4bd170fa873df59d0ee
Ryan Osmundson <rosmundson@outlook.com>
2022-04-12 00:06:05 -0600

    commiting changes for the day

--- index.js
+++ index.js
@@ -3,108 +3,94 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("Page has loaded");
 });
 
+//Card Container Elements:
+const welcome = document.createElement("h3")
+const cards = document.createElement("div");
+const factsTitle = document.createElement("h3");
+const factsContent = document.createElement("p");
+const button = document.createElement("button") 
+
 //Main Node grabbers:
 const BASE_URL = 'http://localhost:3000';
+const homeButton = document.querySelector("#home");
+const aboutPage = document.querySelector("#about-page")
+const homePage = document.querySelector("#home-page")
+button.textContent = "Read the Facts";
+const aboutButton = document.querySelector("#about");
+const welcomeCard = document.getElementById('welcome-card');
 const factsContainer = document.getElementById("facts-container");
 const factsCards = document.getElementsByClassName("facts-cards");
-const welcome = document.getElementById("welcome");
-const welcomeButton = document.getElementById("welcome-button") 
-const cardsButton = document.createElement("button");
-cardsButton.textContent = "Read the Facts";
-
-
-//const homeNav = document.getElementById("home")
-//const awayNav = document.getElementById("about-me")
 //const userStoryContainer = document.getElementById("")
-//----------------------------------------------------------------
 
-
+function createWelcomeCard(welcome) {
+    welcome.id = "welcome-card";
+    welcome.className = "facts-cards";
+    welcome.innerHTML = "<ul><ul>Most people have heard of it.</ul><br><ul>Many feel like they understand it</ul><br><ul>Here are some facts about ADHD you might not have heard!</ul></ul>";
+    factsContainer.append(welcome);
+    factsContainer.appendChild(button);
+    
+}
 function createFactsCards(factoid){
-    const factsCards = document.createElement("div");
     factsCards.id = `fact-card-${factoid.id}`;
     factsCards.className = "facts-cards";
-    const factsTitle = document.createElement("h3");
     factsTitle.textContent = `${factoid.title}`;
-    const factsContent = document.createElement("p");
     factsContent.textContent = `${factoid.fact}`;
-    factsContainer.append(factsCards);
-    factsCards.append(factsTitle, factsContent);
-    factsContainer.appendChild(cardsButton);
-}    
+    factsContainer.append(factsCards, factsTitle, factsContent);
+}
+
 
 function fetchFacts() {
     fetch(BASE_URL + '/TheFacts')
     .then(response => response.json())
-    .then(facts => loadFacts(facts));
+    .then(facts => {
+        loadFacts(facts);
+    })
 }
 
 function loadFacts(facts) {
-    let cards = facts
-    welcomeButton.addEventListener('click', () => {
-        welcome.remove();
-        welcomeButton.remove();
-        let button = document.querySelector('button');
-            for(let i = 0; i < cards.length; i++) {
-                let card = cards[i];
-                //console.log(card);
-                if(card === cards[i]){
-                switch (card.id) {
-                    case 1:
-                        createFactsCards(card);
-                        button = document.querySelector('button')
-                        button.addEventListener('click', () => {
-                                    cards = cards.slice(1);
-                        console.log(factsCards)
-                        let currentFact = factsCards
-                        currentFact.remove()
-                        let nextFact = cards[i];
-                                    console.log(nextFact)
-                                    createFactsCards(nextFact);
-                        })
-                }
-            }
+    createWelcomeCard(welcome);
+    factsContainer.appendChild(button);
+    console.log(facts)
+        for(let i = 0; i < facts.length; i++) {
+            let cards = facts
+            facts.forEach(() => {
+                createFactsCards(facts)
+            })
         }
+    }
+        
+ //factsContainer.appendChild(button);
+        // button.addEventListener('click', () => {
+        //     for(let i = 0; i < cards.length; i++) {
+        //         let card = cards[i]
+        //         console.log(card)
+        //         createFactsCards(card);
+        //         let cardId = cards[i].id
+        //         if(cardId = factsCards[i]){
+        //             console.log(cardId)
+
+//         })
+//     })
+// }
+function navHome() {
+    homeButton.addEventListener('click', () => {
+            console.log('home button clicked')
+
+    });
+};
+
+function navAbout() {
+    aboutButton.addEventListener('click', () => {
+        console.log('about button clicked')
     })
 }
-      
-  
-
-                            
-
-                 
-                    
-
-                            
-       
-
-function removeCard(cards) {
-    console.log(cards);
-}
-        
-        
-        
-        function save() {
-            // for(let i = 0; i < cards.length; i++) {
-            // if(facts[i].id = facts[i].id)
-            // renderFacts(facts[i]);
-            // let currentFact = document.getElementById('fact-card-1');
-            // console.log(currentFact);
-            }
-            
-
-
-        
-            // for(let i = 0; i < cardsButton; i++) {
-            //     cards.forEach(() => console.log(cards));
-            //         let card = facts[i];
-            //         let buttonId = `${card.id}`
-            // }
-
             
 
 
 function initiateFactsApp() {
     fetchFacts();
+    navHome();
+    navAbout();
 }
 
 initiateFactsApp();////END 0F CODE!!!                   
