1b54c0ab1527f63ddfe4c228ea665f4ec4b9a512
Ryan Osmundson <rosmundson@outlook.com>
2022-04-15 18:59:30 -0600

    commiting changes

--- index.js
+++ index.js
@@ -3,78 +3,98 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("Page has loaded");
 });
 
-function initiateFactsApp() {
+const BASE_URL = 'http://localhost:3000';
+const factsContainer = document.getElementById("facts-container");
+const welcome = document.getElementById("welcome")
+const factsButton = document.getElementsByClassName('facts-buttons')
+const button = document.createElement('button');
 
-    function navHome() {
-        homeButton.addEventListener('click', () => {
-            console.log('home button clicked')
-        });
-    };
+function fetchFacts() {  
+    fetch(BASE_URL + '/TheFacts')
+    .then(response => response.json())
+    .then(facts => {
+        facts.forEach(fact => createFactsCards(fact));
+    })
+    }
 
-    function navAbout() {
-        aboutButton.addEventListener('click', () => {
-            console.log('about button clicked')
-        });
-    };
-
-    const BASE_URL = 'http://localhost:3000';
-    const homeButton = document.getElementById("home");
-    const aboutButton = document.getElementById("about");
-    const factsContainer = document.getElementById("facts-container");
-    const welcomeCard = document.createElement("h3");
-    const factsCards = document.createElement("div");
+function createFactsCards(factoid){
+    const factsCard = document.createElement("div");
+    factsCard.id = `fact-card-${factoid.id}`;
+    factsCard.className = "facts-cards";
     const factsTitle = document.createElement("h3");
+    factsTitle.textContent = `${factoid.title}`;
     const factsContent = document.createElement("p");
-    const button = document.createElement("button");
+    const factsButton = document.createElement("button")
+    factsButton.id = `button-${factoid.id}`;
+    factsButton.className = "facts-buttons"
+    factsButton.textContent = "Read Next Fact";
+    factsContent.textContent = `${factoid.fact}`;  
+    factsContainer.append(factsCard);
+    factsCard.append(factsTitle, factsContent);
+    factsCard.append(factsButton)
+    //factsCard.style.display = 'none'
+    changeCards();
+};
 
-    function createFactsCards(fact){
-        welcomeCard.id = "fact-card-0";
-        welcomeCard.className = "facts-cards";
-        welcomeCard.innerHTML = "<div>It's fair to say that ADHD is a hard disorder to understand. Most people know the basics, but there is a lot more going on behind the scenes than most realize. Here are 10 facts you may or may not have known about ADHD!";
-        factsCards.id = `fact-card-${fact.id}`;
-        factsCards.className = "facts-cards";
-        factsTitle.textContent = `${fact.title}`;
-        factsContent.textContent = `${fact.fact}`;
-        button.id = "button"
-        button.class = "cards-buttons"
-        button.textContent = "Read the Facts!"
-   
-        let welcome = welcomeCard
-        factsContainer.append(welcome)
-        factsContainer.appendChild(button)
-
-        button.addEventListener('click', () => {
-            let thisCard = welcome;
-            console.log(thisCard);
-            
-            let thatCard = factsCards;
-            thatCard.append(fact)
-            console.log(thatCard)
-
-
-        })
-    }
-
-    function fetchFacts() {  
-        fetch(BASE_URL + '/TheFacts')
-        .then(response => response.json())
-        .then(facts => {
-            createFactsCards(facts)
-        })
-    }
+function changeCards() {
+    welcome.append(button)
+    button.textContent = "Read the Facts"
+    let cards = document.getElementsByClassName('facts-cards');
     
-    
+    let fact1 = document.getElementById("fact-card-1")
+    // let fact2= document.getElementById("fact-card-2")
+    // let fact3= document.getElementById("fact-card-3")
+    // let fact4= document.getElementById("fact-card-4")
+    // let fact5= document.getElementById("fact-card-5")
+    // let fact6= document.getElementById("fact-card-6")
+    // let fact7= document.getElementById("fact-card-7")
+    // let fact8= document.getElementById("fact-card-8")
+    // let fact9= document.getElementById("fact-card-9")
+    // let fact10= document.getElementById("fact-card-10")
+    button.addEventListener('click', () => {
+        welcome.remove();
+        fact1.style.display = 'block'
 
-    function buttonEvent(){}
+        
+        console.log(cards)
 
-    function hideCard(){}
 
- 
+
+
+
+       
+
+    })
+}
+
+
+
+
+
+
+
+
+function navHome() {
+    const homeButton = document.getElementById("home");
+    homeButton.addEventListener('click', () => {
+        console.log('home button clicked')
+    });
+};
+
+function navAbout() {
+    const aboutButton = document.getElementById("about");
+    aboutButton.addEventListener('click', () => {
+        console.log('about button clicked')
+    });
+};
 //end of code. ititiateFactsApp() up top.
+
+
+function initiateFactsApp() {
     fetchFacts();
     navHome();
     navAbout();
-
+    
 }
 
 initiateFactsApp();////END 0F CODE!!!                   
