840200d52a798385e224db5d473d6dd1a474f344
Ryan Osmundson <rosmundson@outlook.com>
2022-04-20 13:13:23 -0600

    Greate success! home button, about button, facts are all doing what they are supposed to. Next step, create comment form.

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const BASE_URL = 'http://localhost:3000';
-const welcome = document.getElementById("welcome-card");
+const welcome = document.getElementById("welcome");
 const welcomeButton = document.createElement('button');
 const factsContainer = document.getElementById("facts-container");
-const cardButton = document.getElementsByClassName("cards-button")
-const home = document.querySelector("#facts-page")
-
+const cards = document.getElementsBy
+const factsPage = document.getElementById("facts-page")
+const aboutPage = document.getElementById("about-page")
 
 function createFactsCards(facts) {
     welcomeButton.id = "welcome-button";
@@ -17,21 +17,20 @@ function createFactsCards(facts) {
     const factsCards = document.createElement("div");
     factsCards.id = `facts-card-${facts.id}`;
     factsCards.className = "facts-cards";
-    const factsTitle = document.createElement("h3");
+    const br = document.createElement("br")
+    const factsImage = document.createElement('img')
+    factsImage.src = `${facts.image}`
+    factsImage.id = `image-${facts.id}`
+    factsImage.className = "images";
+    const factsTitle = document.createElement("h1");
     factsTitle.textContent = `${facts.title}`;
-    const factsContent = document.createElement("p");
+    const factsContent = document.createElement("h3");
     factsContent.textContent = `${facts.fact}`;
     const cardsButton = document.createElement('button')
-    cardsButton.className = 'cards-button';
-    cardsButton.id = `${facts.id}`
-    cardsButton.textContent = "Read Next Fact"
-    const lineBreak = document.createElement("br");
     welcome.append(welcomeButton);
     factsContainer.append(factsCards);
-    factsCards.append(factsTitle,factsContent);
-    factsCards.append(lineBreak);
-    factsCards.append(cardsButton);
-    factsCards.style.display = "none"
+    factsCards.append( factsTitle,factsImage, factsContent, br, br);
+    factsContainer.style.display = "none";
 }
 
 function fetchFacts() {  
@@ -39,49 +38,37 @@ function fetchFacts() {
     .then(response => response.json())
     .then(facts => {
         facts.forEach(fact => createFactsCards(fact));
-        doStuff(facts);
+        welcomeToFacts(facts)
     })
 }
 
-
-function doStuff(facts) {
-    let cards = facts
-    let buttons = [...cardButton]
-    welcomeButton.addEventListener('click', () => {
-        welcome.remove();
-        let card = document.querySelector(".facts-cards");
-        card.style.display = 'block';
-            
-                buttons.forEach(button => {
-                        button.addEventListener('click', () => {
-                            for(let i = 0; i < cards.length; i++) {
-                                
-                            }
-
-                            card.remove();
-                            card = document.querySelector(".facts-cards");
-                            card.style.display = 'block'
-                            if(card.id === 'facts-card-10'){
-                                factsContainer.innerHTML = "<h2>Thanks For Checking out the facts App!</h2>"
-                            }
-                        })
-                    })
-                })
-            }
-                            
- 
+function welcomeToFacts() {
+    welcomeButton.addEventListener("click", () => {
+        welcome.style.display = "none";
+        factsContainer.style.display = "block";
+        let title = document.querySelector("#title");
+        title.textContent = "Did You Know";
+    })
+}
 
 function navHome() {
     const homeButton = document.getElementById("home");
     homeButton.addEventListener('click', () => {
-        console.log('home button clicked')
+        factsPage.style.display = "none" ? factsPage.style.display = "block" : factsPage.style.display = "block";
+        factsContainer.style.display = "block" ? factsContainer.style.display = "none" : factsContainer.style.display = "none";
+        aboutPage.style.display = "block" ? aboutPage.style.display = "none" : aboutPage.style.display = "none";
+        welcome.style.display = "none" ? welcome.style.display = "block" : welcome.style.display = "block";
+        
     });
 };
 
 function navAbout() {
     const aboutButton = document.getElementById("about");
     aboutButton.addEventListener('click', () => {
-        console.log('about button clicked')
+        aboutPage.style.display = "none" ? aboutPage.style.display = "block" : aboutPage.style.display = "block";
+        factsContainer.style.display = "block" ? factsContainer.style.display = "none" : factsContainer.style.display = "none";
+        factsPage.style.display = "block" ? factsPage.style.display = "none" : factsPage.style.display = "none";
+        title.textContent = ""
     });
 };
 //end of code. ititiateFactsApp() up top.
